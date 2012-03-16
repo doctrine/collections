55eb7d86d4a8b3d95fce4214aa7a384aa9dfd6ba
Benjamin Eberlei <kontakt@beberlei.de>
2012-03-16 23:49:43 +0100

    [DDC-1637] Add docblocks for ExpressionBuilder

--- ExpressionBuilder.php
+++ ExpressionBuilder.php
@@ -31,56 +31,107 @@ use Doctrine\Common\Collections\Expr\Value;
  */
 class ExpressionBuilder
 {
+    /**
+     * @return CompositeExpression
+     */
     public function andX($x = null)
     {
         return new CompositeExpression(CompositeExpression::TYPE_AND, func_get_args());
     }
 
+    /**
+     * @return CompositeExpression
+     */
     public function orX($x = null)
     {
         return new CompositeExpression(CompositeExpression::TYPE_OR, func_get_args());
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function eq($field, $value)
     {
         return new Comparison($field, Comparison::EQ, new Value($value));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function gt($field, $value)
     {
         return new Comparison($field, Comparison::GT, new Value($value));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function lt($field, $value)
     {
         return new Comparison($field, Comparison::LT, new Value($value));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function gte($field, $value)
     {
         return new Comparison($field, Comparison::GTE, new Value($value));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function lte($field, $value)
     {
         return new Comparison($field, Comparison::LTE, new Value($value));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function neq($field, $value)
     {
         return new Comparison($field, Comparison::NEQ, new Value($value));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function isNull($field)
     {
         return new Comparison($field, Comparison::IS, new Value(null));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function in($field, array $values)
     {
         return new Comparison($field, Comparison::IN, new Value($values));
     }
 
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @return Comparison
+     */
     public function notIn($field, array $values)
     {
         return new Comparison($field, Comparison::NIN, new Value($values));
