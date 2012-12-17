4df65cc7822818c51071130963c380445ab788a2
basico <jfcalcerrada@yahoo.es>
2012-12-17 23:01:56 +0000

    Update lib/Doctrine/Common/Collections/ArrayCollection.php

--- ArrayCollection.php
+++ ArrayCollection.php
@@ -120,7 +120,7 @@ class ArrayCollection implements Collection, Selectable
      */
     public function remove($key)
     {
-        if (array_key_exists($key, $this->_elements)) {
+        if (isset($this->_elements[$key]) || array_key_exists($key, $this->_elements)) {
             $removed = $this->_elements[$key];
             unset($this->_elements[$key]);
 
@@ -214,7 +214,7 @@ class ArrayCollection implements Collection, Selectable
      */
     public function containsKey($key)
     {
-        return array_key_exists($key, $this->_elements);
+        return isset($this->_elements[$key]) || array_key_exists($key, $this->_elements);
     }
 
     /**
