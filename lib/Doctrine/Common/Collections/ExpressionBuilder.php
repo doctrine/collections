5e2d9cfe6a3d8eb2906f805a27c27ba3e5f2140c
Benjamin Eberlei <kontakt@beberlei.de>
2014-02-03 23:59:20 +0100

    Add documentation that you should use scalar values.

--- lib/Doctrine/Common/Collections/ExpressionBuilder.php
+++ lib/Doctrine/Common/Collections/ExpressionBuilder.php
@@ -26,6 +26,10 @@ use Doctrine\Common\Collections\Expr\Value;
 /**
  * Builder for Expressions in the {@link Selectable} interface.
  *
+ * Important Notice for interoperable code: You have to use scalar
+ * values only for comparisons, otherwise the behavior of the comparision
+ * may be different between implementations (Array vs ORM vs ODM).
+ *
  * @author Benjamin Eberlei <kontakt@beberlei.de>
  * @since  2.3
  */
