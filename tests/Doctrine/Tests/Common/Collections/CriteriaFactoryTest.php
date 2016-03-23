5cbed7272101514064d0782eb2bcb24fbe372894
Nikola Posa <posa.nikola@gmail.com>
2016-03-23 19:18:27 +0100

    Added test case for nested criteria expressions array.

--- tests/Doctrine/Tests/Common/Collections/CriteriaFactoryTest.php
+++ tests/Doctrine/Tests/Common/Collections/CriteriaFactoryTest.php
@@ -70,9 +70,61 @@ class CriteriaFactoryTest extends \PHPUnit_Framework_TestCase
         $criteria = $this->criteriaFactory->create($criteriaArray);
 
         $where = $criteria->getWhereExpression();
+        $comparisons = $where->getExpressionList();
+
         $this->assertInstanceOf('Doctrine\Common\Collections\Expr\CompositeExpression', $where);
         $this->assertEquals(CompositeExpression::TYPE_OR, $where->getType());
-        $this->assertCount(2, $where->getExpressionList());
+        $this->assertCount(2, $comparisons);
+
+        $this->assertInstanceOf('Doctrine\Common\Collections\Expr\Comparison', $comparisons[0]);
+        $this->assertInstanceOf('Doctrine\Common\Collections\Expr\Comparison', $comparisons[1]);
+    }
+
+    public function testNestedExpressionCreation()
+    {
+        $criteriaArray = array(
+            //(firstName = 'test' OR lastName = 'test' OR nickName = 'test') AND (status = 1 AND age >= 20)
+            'expression' => array(
+                array(
+                    '$or' => array(
+                        array('fld' => 'firstName', 'op' => '=', 'val' => 'test'),
+                        array('fld' => 'lastName', 'op' => '=', 'val' => 'test'),
+                        array('fld' => 'nickName', 'op' => '=', 'val' => 'test'),
+                    ),
+                ),
+                array(
+                    '$and' => array(
+                        array('fld' => 'status', 'op' => '=', 'val' => 1),
+                        array('fld' => 'age', 'op' => '>=', 'val' => 20),
+                    ),
+                ),
+            ),
+        );
+
+        $criteria = $this->criteriaFactory->create($criteriaArray);
+
+        $where = $criteria->getWhereExpression();
+        $whereParts = $where->getExpressionList();
+
+        $this->assertInstanceOf('Doctrine\Common\Collections\Expr\CompositeExpression', $where);
+        $this->assertEquals(CompositeExpression::TYPE_AND, $where->getType());
+        $this->assertCount(2, $whereParts);
+
+        $where1 = $whereParts[0];
+        $this->assertInstanceOf('Doctrine\Common\Collections\Expr\CompositeExpression', $where1);
+        $this->assertEquals(CompositeExpression::TYPE_OR, $where1->getType());
+        $this->assertCount(3, $where1->getExpressionList());
+        foreach ($where1->getExpressionList() as $comparison) {
+            $this->assertInstanceOf('Doctrine\Common\Collections\Expr\Comparison', $comparison);
+        }
+
+        $where2 = $whereParts[1];
+        $this->assertInstanceOf('Doctrine\Common\Collections\Expr\CompositeExpression', $where2);
+        $this->assertEquals(CompositeExpression::TYPE_AND, $where2->getType());
+        $this->assertCount(2, $where2->getExpressionList());
+        foreach ($where2->getExpressionList() as $comparison) {
+            $this->assertInstanceOf('Doctrine\Common\Collections\Expr\Comparison', $comparison);
+        }
     }
 
     public function testExceptionIsRaisedInCaseOfInvalidExpression()
