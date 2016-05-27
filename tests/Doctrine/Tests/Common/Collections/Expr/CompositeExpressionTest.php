46a476a6ac1f614442c7e4b1a6dca44f0b34f892
Tobias Oberrauch <tobias.oberrauch@grolba.com>
2016-05-27 14:51:06 +0200

    Add unit tests for value and composite expression (#86)
    Add unit tests for value and composite expression

---
+++ tests/Doctrine/Tests/Common/Collections/Expr/CompositeExpressionTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Doctrine\Tests\Common\Collections\Expr;
+
+use Doctrine\Common\Collections\Expr\CompositeExpression;
+use Doctrine\Common\Collections\Expr\ExpressionVisitor;
+use Doctrine\Common\Collections\Expr\Value;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @author  Tobias Oberrauch <hello@tobiasoberrauch.com>
+ * @covers  \Doctrine\Common\Collections\Expr\CompositeExpression
+ */
+class CompositeExpressionTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function invalidDataProvider()
+    {
+        return array(
+            array(
+                'expression' => new Value('value'),
+            ),
+            array(
+                'expression' => 'wrong-type',
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     *
+     * @param $expression
+     * @return void
+     */
+    public function testExceptions($expression)
+    {
+        $type = CompositeExpression::TYPE_AND;
+        $expressions = array(
+            $expression,
+        );
+
+        $this->setExpectedException('\RuntimeException');
+        new CompositeExpression($type, $expressions);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetType()
+    {
+        $compositeExpression = $this->createCompositeExpression();
+
+        $expectedType = CompositeExpression::TYPE_AND;
+        $actualType = $compositeExpression->getType();
+
+        $this->assertSame($expectedType, $actualType);
+    }
+
+    /**
+     * @return CompositeExpression
+     */
+    protected function createCompositeExpression()
+    {
+        $type = CompositeExpression::TYPE_AND;
+        $expressions = array(
+            $this->getMock('Doctrine\Common\Collections\Expr\Expression'),
+        );
+
+        $compositeExpression = new CompositeExpression($type, $expressions);
+
+        return $compositeExpression;
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetExpressionList()
+    {
+        $compositeExpression = $this->createCompositeExpression();
+
+        $expectedExpressionList = array(
+            $this->getMock('Doctrine\Common\Collections\Expr\Expression'),
+        );
+        $actualExpressionList = $compositeExpression->getExpressionList();
+
+        $this->assertEquals($expectedExpressionList, $actualExpressionList);
+    }
+
+    /**
+     * @return void
+     */
+    public function testVisitor()
+    {
+        $compositeExpression = $this->createCompositeExpression();
+
+        $visitor = $this->getMockForAbstractClass('Doctrine\Common\Collections\Expr\ExpressionVisitor');
+        $visitor
+            ->expects($this->once())
+            ->method('walkCompositeExpression');
+
+        /** @var ExpressionVisitor $visitor */
+        $compositeExpression->visit($visitor);
+    }
+}
