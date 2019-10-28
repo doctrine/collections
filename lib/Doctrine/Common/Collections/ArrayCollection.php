763af60cf4512ad28e3ab3ce8d55bc9e56dd6155
Illia Somov <illia.somov@kntele.com>
2019-10-28 09:46:50 +0200

    fix #210 by including psalm-* annotations to ArrayCollection#map() method

--- lib/Doctrine/Common/Collections/ArrayCollection.php
+++ lib/Doctrine/Common/Collections/ArrayCollection.php
@@ -312,6 +312,10 @@ class ArrayCollection implements Collection, Selectable
      * {@inheritDoc}
      *
      * @return static
+     *
+     * @psalm-template U
+     * @psalm-param Closure(T=):U $func
+     * @psalm-return static<TKey, U>
      */
     public function map(Closure $func)
     {
