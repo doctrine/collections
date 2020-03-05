4032a1f7fd9e57d08affe8ecbb548baac650b7e6
Oleg Andreyev <oleg.andreyev@ecentria.com>
2020-03-05 13:06:45 +0200

    Utilizing Expression::visit method instead of switch-case in ExpressionVisitor::dispatch

--- lib/Doctrine/Common/Collections/Expr/ExpressionVisitor.php
+++ lib/Doctrine/Common/Collections/Expr/ExpressionVisitor.php
@@ -43,15 +43,6 @@ abstract class ExpressionVisitor
      */
     public function dispatch(Expression $expr)
     {
-        switch (true) {
-            case $expr instanceof Comparison:
-                return $this->walkComparison($expr);
-            case $expr instanceof Value:
-                return $this->walkValue($expr);
-            case $expr instanceof CompositeExpression:
-                return $this->walkCompositeExpression($expr);
-            default:
-                throw new RuntimeException('Unknown Expression ' . get_class($expr));
-        }
+        return $expr->visit($this);
     }
 }
