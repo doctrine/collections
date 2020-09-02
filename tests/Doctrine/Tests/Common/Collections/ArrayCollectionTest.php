0f0c5133c88639943a865b5b09d86286161bc01e
Sebastian Kuhnow <sebastian.kuhnow@gmail.com>
2020-09-02 16:44:58 +0200

    Add method to create a new collection with fixed index

--- tests/Doctrine/Tests/Common/Collections/ArrayCollectionTest.php
+++ tests/Doctrine/Tests/Common/Collections/ArrayCollectionTest.php
@@ -12,8 +12,29 @@ use Doctrine\Common\Collections\Collection;
  */
 class ArrayCollectionTest extends BaseArrayCollectionTest
 {
+    /**
+     * @param array $elements
+     *
+     * @return Collection|ArrayCollection
+     */
     protected function buildCollection(array $elements = []) : Collection
     {
         return new ArrayCollection($elements);
+    }
+
+    public function testRebuildKeys()
+    {
+        $collection = $this->buildCollection([
+            ['foo' => 1, 'bar' => 2],
+            ['foo' => 2, 'bar' => 4],
+            ['foo' => 2, 'bar' => 3],
+        ]);
+
+        $collection->remove(1);
+
+        $this->assertEquals([
+            ['foo' => 1, 'bar' => 2],
+            ['foo' => 2, 'bar' => 3],
+        ], $collection->rebuildKeys()->toArray());
     }
 }
